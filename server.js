263a6de24b64b1d458866aa0782e4a30ef94bdfc
Galal <galalex4780@gmail.com>
2026-06-14 16:53:29 +0100

    clean server fix

--- server.js
+++ server.js
@@ -10,62 +10,56 @@ app.use(express.static("public"));
 
 const players = {};
 
+// 🔌 kapcsolat
 io.on("connection", (socket) => {
     console.log("Játékos csatlakozott:", socket.id);
 
     // új játékos
     players[socket.id] = {
-    x: 100 + Math.random() * 200,
-    y: 100 + Math.random() * 200,
-    name: "Player"
-};
-socket.on("setName", (name) => {
-    if (players[socket.id]) {
-        players[socket.id].name = name;
-        io.emit("players", players);
-    }
-});
-    // küldjük mindenkinek
-    io.emit("players", players);
+        x: 100 + Math.random() * 200,
+        y: 100 + Math.random() * 200,
+        name: "Player"
+    };
 
+    // név beállítás
+    socket.on("setName", (name) => {
+        if (players[socket.id]) {
+            players[socket.id].name = name;
+            io.emit("players", players);
+        }
+    });
+
+    // mozgás
     socket.on("move", (data) => {
         if (players[socket.id]) {
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
-
             io.emit("players", players);
         }
     });
-socket.on("chat", (msg) => {
-    const player = players[socket.id];
 
-    if (!player) return;
+    // chat
+    socket.on("chat", (msg) => {
+        const player = players[socket.id];
+        if (!player) return;
 
-    io.emit("chat", {
-        id: socket.id,
-        name: player.name,
-        msg: msg
+        io.emit("chat", {
+            id: socket.id,
+            name: player.name,
+            msg: msg
+        });
     });
-});
+
+    // disconnect
     socket.on("disconnect", () => {
         delete players[socket.id];
         io.emit("players", players);
     });
 });
 
-server.listen(3000, () => {
-    console.log("Szerver fut: http://localhost:3000");
-});const PORT = process.env.PORT || 3000;
+// 🌍 PORT (Render kompatibilis)
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log("Server running on port " + PORT);
-});
-io.on("connection", (socket) => {
-
-    console.log("Csatlakozott:", socket.id);
-
-    socket.on("move", (data) => {
-        socket.broadcast.emit("move", data);
-    });
-
 });
